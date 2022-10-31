ed683a117b255e3b1c3dbe8bf9b901557c080a85
Tuestag <81398090+Tuestag@users.noreply.github.com>
2022-10-31 01:55:48 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import cv2
 from PIL import Image
 import numpy as np
 import os
-
+#
 from const import CLASSES, COLORS
 from settings import DEFAULT_CONFIDENCE_THRESHOLD, DEMO_IMAGE, MODEL, PROTOTXT
 
